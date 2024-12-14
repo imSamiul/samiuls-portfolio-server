a32ea1d36e3270c1063c7ed7ad182e1518e3b15b
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-12-14 11:01:10 +0600

    Add localhost:3000 to CORS allowed origins for development

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ app.use(
       'https://8295-103-180-245-251.ngrok-free.app',
       'http://192.168.0.174:3001',
       'http://localhost:4173',
+      'http://localhost:3000',
     ], // Specify the allowed origin (React app)
     methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Allowed HTTP methods
     credentials: true, // Allow cookies, authorization headers, etc.
