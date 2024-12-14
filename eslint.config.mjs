d9fabde488b7f5400753648774262e913957b4de
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-12-14 07:29:51 +0600

    Update environment variables, enhance ESLint configuration, and modify package scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,40 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default [{
+    ignores: ["**/dist/"],
+}, ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-undef": "off",
+        "require-yield": "off",
+        "@typescript-eslint/no-unused-expressions": "warn",
+        "@typescript-eslint/no-require-imports": "off",
+    },
+}];
